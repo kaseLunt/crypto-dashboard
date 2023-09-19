c9301630cc5e4941a925640e84109f854a5a07f7
kase <kllunt@asu.edu>
2023-09-18 20:18:34 -0700

    multi-asset support. Currently btc and eth

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -2,40 +2,22 @@ import React from 'react';
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
 
-const Chart = ({ data }) => {
-  // Implementing an error state to catch issues within this component
-  const [hasError, setHasError] = React.useState(false);
+export default function Chart({ data, coinType }) {
+  const transformedData = {
+    labels: data.prices.map((item) => new Date(item[0]).toLocaleDateString()),
+    datasets: [
+      {
+        label: `${coinType} Price`,
+        borderColor: 'rgb(75, 192, 192)',
+        data: data.prices.map((item) => item[1]),
+      },
+    ],
+  };
 
-  try {
-    // Transforming data to fit charting library expectations
-    const labels = data.map((point) => new Date(point[0]).toLocaleDateString());
-    const dataSet = data.map((point) => point[1]);
-
-    const chartData = {
-      labels,
-      datasets: [
-        {
-          label: 'Bitcoin Price in USD',
-          data: dataSet,
-          fill: false,
-          borderColor: 'rgb(75, 192, 192)',
-        },
-      ],
-    };
-
-    console.log('Transformed Data:', chartData);
-
-    if (!chartData.labels || !chartData.datasets) {
-      throw new Error("Transformed data is missing 'labels' or 'datasets'");
-    }
-
-    // Rendering the Line chart
-    return <Line data={chartData} />;
-  } catch (error) {
-    console.error('An error occurred in the Chart component:', error);
-    setHasError(true);
-    return <div>Error occurred while rendering the chart: {error.message}</div>;
-  }
-};
-
-export default Chart;
+  return (
+    <div>
+      <h2>{coinType} Chart</h2>
+      <Line data={transformedData} />
+    </div>
+  );
+}
--- src/components/DataLoader.js
+++ src/components/DataLoader.js
@@ -1,33 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { fetchCoinData } from '../utils/api'; // Assuming this utility function exists
+import { fetchCoinData } from '../utils/api'; // Import your api function
 import Chart from './Chart';
+import 'chart.js/auto';
 
 export default function DataLoader() {
-  // State variables for fetched data and error handling
-  const [data, setData] = useState({ prices: [] });
-  const [error, setError] = useState(null);
+  const [bitcoinData, setBitcoinData] = useState(null);
+  const [ethereumData, setEthereumData] = useState(null);
 
   useEffect(() => {
-    fetchCoinData('bitcoin', 'usd', 30)
-      .then((response) => {
-        console.log('Data:', response);
-        setData(response);
-      })
-      .catch((fetchError) => {
-        console.error('Error:', fetchError);
-        setError(fetchError);
-      });
+    (async () => {
+      const btcData = await fetchCoinData('bitcoin', 'usd', 30); // using api.js function
+      const ethData = await fetchCoinData('ethereum', 'usd', 30); // using api.js function
+      setBitcoinData(btcData);
+      setEthereumData(ethData);
+    })();
   }, []);
 
-  // Display error message if something goes wrong during data fetching
-  if (error) {
-    return <div>Error occurred while fetching data: {error.message}</div>;
-  }
-
-  // Check for data in the 'prices' array for conditional rendering
-  return data.prices && data.prices.length > 0 ? (
-    <Chart data={data.prices} />
-  ) : (
-    <div>Loading...</div>
+  return (
+    <div>
+      {bitcoinData && <Chart data={bitcoinData} coinType="Bitcoin" />}
+      {ethereumData && <Chart data={ethereumData} coinType="Ethereum" />}
+    </div>
   );
 }
